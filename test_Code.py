4f9ecb64e4d74b00be8bbc3c2045dfd13e9b29ae
onlugassy <oneg.lugassy@gmail.com>
2020-03-15 16:04:33 +0200

    linting fixes

--- test_Code.py
+++ test_Code.py
@@ -8,7 +8,8 @@ import Code
 
 class TestCode(unittest.TestCase):
     def test_get_all_characters(self):
-        self.assertEqual(str(len(Code.get_all_characters())), Code.get_all_characters()[-1][0])
+        self.assertEqual(str(len(Code.get_all_characters())),
+                         Code.get_all_characters()[-1][0])
 
     if __name__ == '__main__':
         unittest.main()
